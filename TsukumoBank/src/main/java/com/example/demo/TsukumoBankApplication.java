cc3653c319f7f617965767f9adc72f804bfa0954
github kazuma-satake <kazuma-satake@kccs.co.jp>
2025-06-19 13:11:02 +0900

    初回コミット

---
+++ TsukumoBank/src/main/java/com/example/demo/TsukumoBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsukumoBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsukumoBankApplication.class, args);
+	}
+
+}
