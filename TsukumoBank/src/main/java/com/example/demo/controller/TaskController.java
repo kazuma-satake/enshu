8ffc5464a3c359f70206f4ee13377867a5d6fd4b
github mnt-izw <manato-aizawa@kccs.co.jp>
2025-06-20 13:30:41 +0900

    ログイン機能：画面遷移を追加

--- TsukumoBank/src/main/java/com/example/demo/controller/TaskController.java
+++ TsukumoBank/src/main/java/com/example/demo/controller/TaskController.java
@@ -1,8 +1,17 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.form.LoginForm;
 
 @Controller
 public class TaskController {
+	
+	@PostMapping("select-operation")
+	public String enterSelectOperation(@ModelAttribute LoginForm form) {
+		return "select-operation";
+	}
 
 }
