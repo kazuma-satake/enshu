4072092c49d67a3527efe679c908676a531591f9
github mnt-izw <manato-aizawa@kccs.co.jp>
2025-06-20 13:55:10 +0900

    ログオン画面：機能選択ページへの遷移を追加

--- TsukumoBank/src/main/java/com/example/demo/controller/TaskController.java
+++ TsukumoBank/src/main/java/com/example/demo/controller/TaskController.java
@@ -4,14 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.example.demo.form.LoginForm;
+import com.example.demo.form.NewRegister;
 
 @Controller
 public class TaskController {
 	
 	@PostMapping("select-operation")
-	public String enterSelectOperation(@ModelAttribute LoginForm form) {
+	public String registerInfo(@ModelAttribute NewRegister form) {
 		return "select-operation";
 	}
-
 }
--- TsukumoBank/src/main/java/com/example/demo/form/LoginForm.java
+++ TsukumoBank/src/main/java/com/example/demo/form/LoginForm.java
@@ -4,8 +4,6 @@ import lombok.Data;
 
 @Data
 public class LoginForm {
-	
 	private String userId;
-	
 	private String password;
 }
